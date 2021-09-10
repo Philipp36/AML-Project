ceaee353ce25005208081a1d8b89b2aa250a4996
Philipp Steichen <p.steichen36@gmail.com>
2021-09-10 17:24:00 +0200

    binary vector for finer grained loss

--- lightningTrainer.py
+++ lightningTrainer.py
@@ -19,12 +19,13 @@ class trainerLightning(pl.LightningModule):
         image, heat_resized, truth, box = batch
         image, heat_resized, truth, box = image.to(self.dev), heat_resized.to(self.dev), truth.to(self.dev), box.to(self.dev)
         heat_pred, label_pred = self.model(image)
-        #mAP = meanAP(box, heat_pred, label_pred, truth)
-        #self.log("mAP/Train", mAP)
-        loss1 = self.lossBoxes(heat_pred.double(), heat_resized.double())
+        # mAP = meanAP(box, heat_pred, label_pred, truth)
+        # self.log("mAP/Train", mAP)
+        bin_vec = truth != 3
+        loss1 = self.lossBoxes(heat_pred.double()[bin_vec], heat_resized.double()[bin_vec])
         loss2 = self.lossLabels(label_pred, truth)
-        self.log("HeatMapLoss/Train", loss1, on_step=True, on_epoch=True)    # TODO: switch to mean over epoch trough on_epoch=True?
-        self.log("LabelLoss/Train", loss2, on_step=True, on_epoch=True)      # TODO: switch to mean over epoch
+        self.log("HeatMapLoss/Train", loss1, on_step=True, on_epoch=True, logger=True)    # TODO: switch to mean over epoch trough on_epoch=True?
+        self.log("LabelLoss/Train", loss2, on_step=True, on_epoch=True, logger=True)      # TODO: switch to mean over epoch
         LOSS = loss1 + loss2
         return LOSS
 
@@ -32,8 +33,8 @@ class trainerLightning(pl.LightningModule):
         image, heat_resized, truth, box = batch
         image, heat_resized, truth, box = image.to(self.dev), heat_resized.to(self.dev), truth.to(self.dev), box.to(self.dev)
         heat_pred, label_pred = self.model(image)
-        #mAP = meanAP(box, heat_pred, label_pred, truth)
-        #self.log("mAP/Test", mAP)
+        # mAP = meanAP(box, heat_pred, label_pred, truth)
+        # self.log("mAP/Test", mAP)
         loss1 = self.lossBoxes(heat_pred.double(), heat_resized.double())
         loss2 = self.lossLabels(label_pred, truth)
         self.log("HeatMapLoss/Test", loss1, on_step=True, on_epoch=True)     # TODO: log val accuracy (mean average precision) instead of loss
