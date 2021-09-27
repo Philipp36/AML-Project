27e05fd7e73390bb1333d65e3990c90a7a3714bf
Philipp Steichen <p.steichen36@gmail.com>
2021-09-27 22:42:44 +0200

    adjusted demo program to fit heatmap prediction

--- demo.py
+++ demo.py
@@ -1,47 +1,39 @@
 import argparse
-import sys
-import yaml
-
-import numpy as np
-import torch
 import logging
-
-import helperFunctions
+import torch
+import yaml
+from matplotlib import pyplot as plt
 from dataLoader import createDataset_300W_LP
-from helperFunctions import *
-from model import model1, AmerModel
-
+from model import AmerModel
+from transformations import RandomRotation, RandomChoice, VerticalFlip, HorizontalFlip, CenterCrop
 
 ################################################
 
-
 def main():
-
-    transforms = helperFunctions.RandomChoice(transforms=[
-        #helperFunctions.RandomRotationWithBox(degrees=(-90, 90)),
-        #helperFunctions.RandomVerticalFlipWithBox(),
-        #helperFunctions.RandomHorizontalFlipWithBox(),
-        helperFunctions.CenterCropWithBox(size=(348, 348))
-    ], p=1)
     pretrained = config['model']['pretrained']
     model_path = config['model']['model_path']
-    dev = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
-    logging.info(torch.cuda.is_available())
-    torch.cuda.empty_cache()
+    dev = torch.device('cpu')
+    transforms = RandomChoice(transforms=[
+        RandomRotation(degrees=(-90, 90)),
+        VerticalFlip(),
+        HorizontalFlip(),
+        CenterCrop(size=(425, 425))
+    ], p=0.4)
     train_dataloader, test_dataloader, eval_dataloader = \
-        createDataset_300W_LP(**config['dataset'],
-                              train_batch=config['train']['batch_size'], test_batch=config['test']['batch_size'],
+        createDataset_300W_LP(**config['dataset'], train_batch=config['train']['batch_size'],
+                              test_batch=config['test']['batch_size'],
                               conf_data_loading_train=config['train']['data_loading'],
                               conf_data_loading_test=config['test']['data_loading'], transforms=transforms)
-    print("#Train Batches:", len(train_dataloader), "#Test Batches:", len(test_dataloader), "#Eval Batches:",
-          len(eval_dataloader))
+    print("#Train Batches:", len(train_dataloader),
+          "#Test Batches:", len(test_dataloader),
+          "#Eval Batches:", len(eval_dataloader))
 
     ################################################
 
     if pretrained:
         print("-> Load trained model...")
         try:
-            model = torch.load(model_path)
+            model = torch.load(model_path, map_location=dev)
         except Exception as e:
             print(e)
             raise ValueError('Model not found!')
@@ -49,59 +41,32 @@ def main():
         print("-> Create New Model...")
         model = AmerModel()
 
-    ################################################
+    if torch.cuda.is_available():
+        model.to(dev)
 
-################################################
+    ################################################
 
     for i in range(0, len(test_dataloader)):
 
         with torch.no_grad():
             iterator = iter(test_dataloader)
 
-            image, truth_heatMap_resized, truth, box, imageOrig, truth_heatMap_origsize = iterator.next()
-            #image, heat_resized, truth, box = iterator.next()
+            image, truth, heat = iterator.next()
+            image, truth, heat = image.to(dev), truth.to(dev), heat.to(dev)
 
             heat_pred, label_pred = model(image)
 
             for i in range(0, heat_pred.shape[0]):
-                #print(truth[i], torch.argmax(label_pred[i]))
-                #print(box[i])
-                #print(heat_pred[i])
-                #print("")
+                image1 = image[i]
+                image1 = image1.swapaxes(0, 1)
+                image1 = image1.swapaxes(1, 2)
+                heat1 = heat[i]
+                heat_pred1 = heat_pred[i]
 
-                image = image[i]
-                image = image.swapaxes(0, 1)
-                image = image.swapaxes(1, 2)
-                imageOrig = imageOrig[i]
-                imageOrig = imageOrig.swapaxes(0, 1)
-                imageOrig = imageOrig.swapaxes(1, 2)
-
-                drawSample(image, imageOrig, box[i], heat_pred[i], originalSize=False)
-
-
-
-            """
-            image = image[0]
-            image = image.swapaxes(0, 1)
-            image = image.swapaxes(1, 2)
-            imageOrig = imageOrig[0]
-            imageOrig = imageOrig.swapaxes(0, 1)
-            imageOrig = imageOrig.swapaxes(1, 2)
-            heat_pred = heat_pred.reshape((-1, 448, 448))
-            box_pred = getBoxFromHeatMap(heat_pred)
-            box = box[0]
-            box_pred = box_pred[0]
-            label_pred = label_pred[0]
-            truth = truth[0]
-            truth_heatMap_resized = truth_heatMap_resized[0]
-            truth_heatMap_origsize = truth_heatMap_origsize[0]
-            print("True Label:  ", int(truth))
-            print("Predicted Label:  ", int(torch.argmax(label_pred)))
-            # You can decide wheter to plot the original size image, or the resized
-            drawSample(image, imageOrig, box, box_pred, originalSize = True)
-            #drawSample(truth_heatMap_resized, truth_heatMap_origsize, box, box_pred, originalSize=False)
-            """
-################################################
+                plt.imshow(image1)
+                plt.imshow(heat1, alpha=0.3, cmap="Greys")
+                plt.imshow(heat_pred1, alpha=0.3, cmap="Reds")
+                plt.show()
 
 
 if __name__ == '__main__':
